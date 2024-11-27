cf81541e7ebf1098ce9941d78f48a9350e1c508a
mohammedfaizalknd <mohammedfaizalknd@gmail.com>
2024-11-27 17:39:24 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-xl font-bold text-blue-800 text-center'>Hello World</div>
+    </>
+  )
+}
+
+export default App
